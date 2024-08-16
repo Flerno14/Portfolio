150b8cb7c6926779879685afcfdd1f8fdd66ea82
Guilherme Melnik Trombetta <melnik.trombetta@gmail.com>
2024-08-16 17:09:03 -0300

    Versao1

---
+++ trimestres/trimestres.js
@@ -0,0 +1,63 @@
+//----1°trimestre----
+
+function voltarParaInicio1() {
+    location.href = '../index.html';
+}
+function irParaHumanas1() {
+    location.href = '../matérias_1_trimestre/humanas_1.html';
+}
+function irParaLinguagens1() {
+    location.href = '../matérias_1_trimestre/linguagens_1.html';
+}
+function irParaMatematica1() {
+    location.href = '../matérias_1_trimestre/matematica_1.html';
+}
+function irParaNatureza1() {
+    location.href = '../matérias_1_trimestre/natureza_1.html';
+}
+function irParaTecnico1() {
+    location.href = '../matérias_1_trimestre/tecnico_1.html';
+}
+
+//----2°trimestre----
+
+function voltarParaInicio2() {
+    location.href = '../index.html';
+}
+function irParaHumanas2() {
+    location.href = '../matérias_2_trimestre/humanas_2.html';
+}
+function irParaLinguagens2() {
+    location.href = '../matérias_2_trimestre/linguagens_2.html';
+}
+function irParaMatematica2() {
+    location.href = '../matérias_2_trimestre/matematica_2.html';
+}
+function irParaNatureza2() {
+    location.href = '../matérias_2_trimestre/natureza_2.html';
+}
+function irParaTecnico2() {
+    location.href = '../matérias_2_trimestre/tecnico_2.html';
+}
+
+//----3°trimestre----
+
+function voltarParaInicio3() {
+    location.href = '../index.html';
+}
+function irParaHumanas3() {
+    location.href = '../matérias_3_trimestre/humanas_3.html';
+}
+function irParaLinguagens3() {
+    location.href = '../matérias_3_trimestre/linguagens_3.html';
+}
+function irParaMatematica3() {
+    location.href = '../matérias_3_trimestre/matematica_3.html';
+}
+function irParaNatureza3() {
+    location.href = '../matérias_3_trimestre/natureza_3.html';
+}
+function irParaTecnico3() {
+    location.href = '../matérias_3_trimestre/tecnico_3.html';
+}
+
